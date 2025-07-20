26ca95702e7031db4cc6519df74c21d402f39c79
Satwik Shirpurwar <149239140+satwik-shirpurwar@users.noreply.github.com>
2025-07-21 01:04:29 +0530

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    proxy: {
+      // All requests from your app starting with /api will be sent to your backend
+      '/api': {
+        target: 'http://localhost:4000', // Your backend server URL
+        changeOrigin: true,
+      }
+    }
+  },
 })
