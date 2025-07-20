c5f594b98fdc548330a96d6f971267f7003d9303
Satwik Shirpurwar <149239140+satwik-shirpurwar@users.noreply.github.com>
2025-07-21 01:33:45 +0530

    Delete frontend/src/services directory

--- frontend/src/services/apiClient.js
+++
@@ -1,9 +0,0 @@
-// File path: frontend/src/services/apiClient.js
-
-import axios from 'axios';
-
-const apiClient = axios.create({
-  baseURL: process.env.VITE_API_URL, // Use VITE_ prefix for Vite projects
-});
-
-export default apiClient;
