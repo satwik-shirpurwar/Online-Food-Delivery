6f1e27bcce2f3bcbd23fdaba06f1baa7d9006758
Satwik Shirpurwar <149239140+satwik-shirpurwar@users.noreply.github.com>
2025-07-21 22:24:32 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from 'react'
-import Header from '../../components/Header/Header'
-import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import AppDownload from '../../components/AppDownload/AppDownload'
+import React, { useState } from 'react';
+import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
-
-  const [category,setCategory] = useState("All")
-
+  const [category, setCategory] = useState("All");
   return (
-    <>
-      <Header/>
-      <ExploreMenu setCategory={setCategory} category={category}/>
-      <FoodDisplay category={category}/>
-      <AppDownload/>
-    </>
-  )
-}
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <AppDownload />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
