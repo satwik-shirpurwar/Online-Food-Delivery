b282a7425534d77969bf54d19ceb94288ab39ad6
Satwik Shirpurwar <149239140+satwik-shirpurwar@users.noreply.github.com>
2025-07-21 11:05:29 +0530

    Add files via upload

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -5,14 +5,11 @@ const foodRouter = express.Router();
 
 //Image Storage Engine (Saving Image to uploads folder & rename it)
 
-const storage = multer.diskStorage({
-    destination: 'uploads',
-    filename: (req, file, cb) => {
-        return cb(null,`${Date.now()}${file.originalname}`);
-    }
-})
+const storage = multer.memoryStorage(); // ✅ Vercel-compatible
+const upload = multer({ storage: storage });
 
-const upload = multer({ storage: storage})
+
+
 
 foodRouter.get("/list",listFood);
 foodRouter.post("/add",upload.single('image'),addFood);
