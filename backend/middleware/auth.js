236123ad67e7985d1470ef5aca94dde90f1b2e09
Satwik Shirpurwar <149239140+satwik-shirpurwar@users.noreply.github.com>
2025-07-21 22:20:18 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,15 +3,15 @@ import jwt from 'jsonwebtoken';
 const authMiddleware = async (req, res, next) => {
     const { token } = req.headers;
     if (!token) {
-        return res.json({success:false,message:'Not Authorized Login Again'});
+        return res.status(401).json({ success: false, message: 'Not Authorized. Login Again.' });
     }
     try {
-        const token_decode =  jwt.verify(token, process.env.JWT_SECRET);
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET);
         req.body.userId = token_decode.id;
         next();
     } catch (error) {
-        return res.json({success:false,message:error.message});
+        return res.status(401).json({ success: false, message: error.message });
     }
-}
+};
 
 export default authMiddleware;
