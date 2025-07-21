39d700e622d0bae99c55c188a90e382b4a34008d
Satwik Shirpurwar <149239140+satwik-shirpurwar@users.noreply.github.com>
2025-07-21 22:25:49 +0530

    Update FoodItem.jsx

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,35 +1,33 @@
-import React, { useContext, useState } from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../Context/StoreContext';
+import React, { useContext } from 'react';
+import './FoodItem.css';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
-const FoodItem = ({ image, name, price, desc , id }) => {
-
-    const [itemCount, setItemCount] = useState(0);
-    const {cartItems,addToCart,removeFromCart,url,currency} = useContext(StoreContext);
+const FoodItem = ({ image, name, price, desc, id }) => {
+    const { cartItems, addToCart, removeFromCart, currency } = useContext(StoreContext);
 
     return (
         <div className='food-item'>
             <div className='food-item-img-container'>
-                <img className='food-item-image' src={url+"/images/"+image} alt="" />
+                <img className='food-item-image' src={image} alt={name} />
                 {!cartItems[id]
-                ?<img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
-                :<div className="food-item-counter">
-                        <img src={assets.remove_icon_red} onClick={()=>removeFromCart(id)} alt="" />
+                    ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="Add" />
+                    : <div className="food-item-counter">
+                        <img src={assets.remove_icon_red} onClick={() => removeFromCart(id)} alt="Remove" />
                         <p>{cartItems[id]}</p>
-                        <img src={assets.add_icon_green} onClick={()=>addToCart(id)} alt="" />
+                        <img src={assets.add_icon_green} onClick={() => addToCart(id)} alt="Add more" />
                     </div>
                 }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
-                    <p>{name}</p> <img src={assets.rating_starts} alt="" />
+                    <p>{name}</p> <img src={assets.rating_starts} alt="Rating" />
                 </div>
                 <p className="food-item-desc">{desc}</p>
                 <p className="food-item-price">{currency}{price}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default FoodItem
+export default FoodItem;
